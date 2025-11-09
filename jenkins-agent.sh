9c0f74a8ed6328a0dfe2dcf8a17f3f96061ebc17
sriharibandi <shreharii@gmail.com>
2025-11-09 13:49:27 +0530

    terraform-aws-eks

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -12,3 +12,23 @@ xfs_growfs /var/tmp
 xfs_growfs /var
 
 yum install java-21-openjdk -y
+
+# Terraform Installation
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+
+# NodeJs installation
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+yum install zip -y
+
+
+# docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
